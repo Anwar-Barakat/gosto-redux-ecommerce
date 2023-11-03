15a33406d094c63c6f98db7d0fd00a68ecb2dc89
Anwar-Barakat <brkatanwar0@gmail.com>
2023-11-03 14:16:46 +0300

    added plugin-proposal-private-property package

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import Hero from "./hero/Hero";
 import Card from "./hero/Card";
 import Product from "./product/Product";
-import Banner from "./banner/Banner";
 import TopProduct from "./topProduct/TopProduct";
 import Price from "./price/Price";
 import Testimonials from "./testimonial/Testimonials";
 import Blog from "./blog/Blog";
+import Banner from "./banner/Banner";
 
 const Home = () => {
   return (
